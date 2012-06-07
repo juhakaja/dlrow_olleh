a4fb74890a57b65d3bf0ffc7e6c213351f4eb95c
Juha Kajava <juhakaja@gmail.com>
2012-06-07 20:46:59 +0900

    Add comments to hello.rb.

--- lib/hello.rb
+++ lib/hello.rb
@@ -1,4 +1,11 @@
 class Hello
+  # Example:
+  # >> Hello.world :finnish
+  #   => Terve, Maailma!
+  #
+  # Arguments:
+  #   language: (Symbol)
+
   def self.world(language = :english)
     translator = Translator.new(language)
     translator.world
